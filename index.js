bb4a8e9cc3cc48061bdcc9afff3668c3d024b465
ARod88 <alma.rodriguez72@yahoo.com>
2023-03-24 15:44:38 -0700

    final solution

--- index.js
+++ index.js
@@ -1,72 +1,51 @@
-//DO NOT RENAME THESE VARIABLES, THEY ARE USED TO RENDER THINGS
-let ride1PowerOn = false
-let ride2PowerOn = false
-let ride3PowerOn = false
-let ride4PowerOn = false
-let greenWireConnected = true
-let yellowWireConnected = true
-let redWireConnected = true
-let waterIsOn = true
-//Everything below is fair game for you to power on the rides
+let ride1PowerOn = false;
+let ride2PowerOn = false;
+let ride3PowerOn = false;
+let ride4PowerOn = false;
+let greenWireConnected = true;
+let yellowWireConnected = true;
+let redWireConnected = true;
+let waterIsOn = true;
 
-//Ride 1 works when:
-    // green wire is connected
-    if (greenWireConnected) {
-        ride1PowerOn=true}
-
-
-
-//Ride 2 works when 
-    //green and yellow wires are connected
-if (greenWireConnected&&yellowWireConnected) {
-    ride2PowerOn=true
+// Power on Ride 1 if green wire is connected
+if (greenWireConnected) {
+  ride1PowerOn = true;
 }
 
-//Ride 3 works when:
-    // green and red wires are connected
-    // water is on 
-    // yellow wire is not connected
-if (greenWireConnected&&redWireConnected) {
-    waterIsOn=true
+// Power on Ride 2 if green and yellow wires are connected
+if (greenWireConnected && yellowWireConnected) {
+  ride2PowerOn = true;
 }
 
-else {
-    console.log("yellow wire is not connected")
+// Power on Ride 3 if green and red wires are connected, water is on, and yellow wire is not connected
+if (greenWireConnected && redWireConnected && waterIsOn && !yellowWireConnected) {
+  ride3PowerOn = true;
 }
 
-ride3PowerOn = true
+// Power on Ride 4 if ride wire is connected
+if (ride4PowerOn) {
+  ride4PowerOn = true;
+}
 
-//Ride 4 has power when:
-    //ride wire is connected
-    
-ride4PowerOn = false
-//Ride 4 is a slot machine
-    //if a 1 is rolled it should return "star"
-    //if a 2 is rolled it should return "car"
-    //if a 3 is rolled it should return "pot of gold"
-    //if a 4 is rolled it should return "clover"
-    //if a 5 is rolled it should return "bowling ball"
-    //if a 6 is rolled it should return "tiger"
-    //if a 7 is rolled it should return "seven"
+// Define the ride4SlotMachine function to return a prize based on the rolled number
 function ride4SlotMachine(rolledNumber){
-    return "nothing"
-}
-
-switch (rolledNumber) {
+    
+  switch (rolledNumber) {
     case 1:
-         return "star"
+      return "star";
     case 2:
-        return "car"
+      return "car";
     case 3:
-        return "pot of gold"
+      return "pot of gold";
     case 4:
-        return "clover"
+      return "clover";
     case 5:
-        return "bowling"
+      return "bowling ball";
     case 6:
-        return "tiger"
+      return "tiger";
     case 7:
-        return "seven"
+      return "seven";
     default:
-        break;
+      break;return "nothing"
+  }
 }
